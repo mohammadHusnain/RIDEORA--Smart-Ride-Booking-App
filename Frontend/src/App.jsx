001e45731ea5f38fe5256cc5c959f5eae158b4bc
Muhammad Husnain <husnainbhinder682@gmail.com>
2025-07-19 23:56:57 +0500

    updated app

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const App = () => {
   return (
-    <div className='bg-red-800 p-4 rounded-lg'>App</div>
+    <div className='bg-red-800 p-4 rounded-lg'>App is here</div>
 
   )
 }
