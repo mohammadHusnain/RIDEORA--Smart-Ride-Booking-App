6be8ed793119bbd26ddc9908c87f2973a2fca8b2
Muhammad Husnain <husnainbhinder682@gmail.com>
2025-08-02 22:18:10 +0500

    updated files

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,21 +1,23 @@
 import React from 'react'
 import { Route , Routes } from 'react-router-dom'
-import Home from './Pages/Home'
+import Start from './Pages/Start'
 import UserLogin from './Pages/UserLogin'
 import UserSignUp from './Pages/UserSignUp'
 import CaptainLogin from './Pages/CaptainLogin'
 import CaptainSignUp from './Pages/CaptainSignUp'
+import Home from './Pages/Home'
 
 const App = () => {
   return (
     <div>
       <Routes>
 
-<Route path='/'  element={<Home />}/>
+<Route path='/'  element={<Start />}/>
 <Route path='/login'  element={<UserLogin />}/>
 <Route path='/signup'  element={<UserSignUp />}/>
 <Route path='/captain-login'  element={<CaptainLogin />}/>
 <Route path='/captain-signup'  element={<CaptainSignUp />}/>
+<Route path='/home'  element={<Home/>}/>
 
       </Routes>
        </div>
--- Frontend/src/Pages/Home.jsx
+++ Frontend/src/Pages/Home.jsx
@@ -1,33 +1,9 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React from 'react'
 
 const Home = () => {
-
   return (
-    <div>
-      <div className='bg-cover bg-center pt-8 h-screen w-full flex justify-between flex-col bg-[url(https://plus.unsplash.com/premium_photo-1731842686156-74895c29a87b?q=80&w=686&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D)]'>
-<img
-  className='w-20 ml-12 '
-  src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTB7JU0LGsmFGSDWCKLoxTryMn6GjpDIhPklWbERkLklVSX4mIhahu1T-_3EMu4NTjp7nk&usqp=CAU"
-  alt="RIDEORA Logo"
-/>
-           <div className='bg-white py-4 px-4 pb-2'> 
-            <h2 className='text-2xl font-bold'>Get Started With RIDEORA</h2>
-          <Link
-            to='/signup'
-            className='flex items-center w-full bg-black text-white justify-center py-3 mt-5 rounded'
-          >
-            Continue
-          </Link>
-           </div>
-      </div>
-    </div>
+    <div>Home</div>
   )
+}
 
-  
-
-};
-
-export default Home;
-
-
+export default Home
