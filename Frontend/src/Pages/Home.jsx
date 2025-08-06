d13d404c40214f81741caf5007cefef25661e8bc
Muhammad Husnain <husnainbhinder682@gmail.com>
2025-08-06 18:38:57 +0500

    updated files

--- Frontend/src/Pages/Home.jsx
+++ Frontend/src/Pages/Home.jsx
@@ -1,15 +1,50 @@
-import React from 'react'
+import React , {useState , useRef} from 'react'
+import {useGSAP} from '@gsap/react'
+import gsap from 'gsap'
+import 'remixicon/fonts/remixicon.css'
+
+
 
 const Home = () => {
 
   const [pickup, setPickup] = useState('')
   const [destination, setDestination] = useState('')
   const [ panelOpen, setPanelOpen ] = useState(false)
+  const panelRef = useRef(null)
+      const panelCloseRef = useRef(null)
+
+
+
 
 
   const submitHandler = (e) => {
     e.preventDefault()
   }
+
+ useGSAP(function () {
+        if (panelOpen) {
+            gsap.to(panelRef.current, {
+                height: '70%',
+               opacity:1
+            })
+
+            gsap.to(panelCloseRef.current,{
+              opacity:1,
+            })
+            
+        } else {
+            gsap.to(panelRef.current, {
+                height: '0%',
+              opacity:0
+            })
+
+               gsap.to(panelCloseRef.current,{
+              opacity:0,
+            })
+
+          }
+        },[panelOpen])
+           
 
   return (
     <div className='h-screen position-relative' >
@@ -22,6 +57,10 @@ const Home = () => {
       <div className=" flex flex-col justify-end h-screen absolute top-0 w-full ">
 
         <div className='h-[30%] p-5 bg-white relative'>
+
+          <h5 ref={panelCloseRef} onClick={()=>{
+            setPanelOpen(false)
+          }} className='absolute top-6 opacity-0 right-6 text-2xl'><i className="ri-arrow-down-s-line"></i></h5>
 
           <h4 className='text-2xl font-semibold'>Find A Trip</h4>
 
@@ -51,7 +90,7 @@ const Home = () => {
           </form>
         </div>
 
-        <div className=' bg-red-500  h-0 '>
+        <div ref={panelRef} className=' bg-red-500 opacity-0 h-0 '>
 
         </div>
 
